0500a929dea0630ee8df4b86d3f7f8eff51a6afd
Joe Baldwin <contact@humboldttechgroup.com>
2013-01-21 14:03:22 -0600

    Code for 1/21/13

---
+++ 01_21_12/MoreBasicControls/MoreBasicControls/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace MoreBasicControls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblClickMessage = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClickMe = new System.Windows.Forms.Button();
+            this.lblLoadMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblClickMessage
+            // 
+            this.lblClickMessage.AutoSize = true;
+            this.lblClickMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClickMessage.Location = new System.Drawing.Point(148, 90);
+            this.lblClickMessage.Name = "lblClickMessage";
+            this.lblClickMessage.Size = new System.Drawing.Size(168, 24);
+            this.lblClickMessage.TabIndex = 0;
+            this.lblClickMessage.Text = "<lblClickMessage>";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(40, 184);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 50);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(195, 184);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 50);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            this.btnExit.MouseHover += new System.EventHandler(this.btnExit_MouseHover);
+            // 
+            // btnClickMe
+            // 
+            this.btnClickMe.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClickMe.Location = new System.Drawing.Point(350, 184);
+            this.btnClickMe.Name = "btnClickMe";
+            this.btnClickMe.Size = new System.Drawing.Size(100, 50);
+            this.btnClickMe.TabIndex = 3;
+            this.btnClickMe.Text = "Click Me!";
+            this.btnClickMe.UseVisualStyleBackColor = true;
+            this.btnClickMe.Click += new System.EventHandler(this.btnClickMe_Click);
+            // 
+            // lblLoadMessage
+            // 
+            this.lblLoadMessage.AutoSize = true;
+            this.lblLoadMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoadMessage.Location = new System.Drawing.Point(147, 22);
+            this.lblLoadMessage.Name = "lblLoadMessage";
+            this.lblLoadMessage.Size = new System.Drawing.Size(170, 24);
+            this.lblLoadMessage.TabIndex = 4;
+            this.lblLoadMessage.Text = "<lblLoadMessage>";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 262);
+            this.Controls.Add(this.lblLoadMessage);
+            this.Controls.Add(this.btnClickMe);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblClickMessage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblClickMessage;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClickMe;
+        private System.Windows.Forms.Label lblLoadMessage;
+    }
+}
+
---
+++ 01_21_12/MoreBasicControls/MoreBasicControls/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MoreBasicControls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Set label to say "This text is set on startup"
+            lblLoadMessage.Text = "This text is set on startup";
+            //clear out the click label so it isn't visible
+            lblClickMessage.Text = string.Empty;
+        }
+
+        private void btnClickMe_Click(object sender, EventArgs e)
+        {
+            //Set the label to say "This text is set on button click"
+            lblClickMessage.Text = "This text is set on button click";
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //Clear out both labels!
+            lblClickMessage.Text = "";
+            lblLoadMessage.Text = string.Empty;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Close the application
+            Application.Exit();
+        }
+
+        private void btnExit_MouseHover(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
