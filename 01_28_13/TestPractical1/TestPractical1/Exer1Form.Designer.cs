78ad734c1ebd1c699139bb40603b01617e8784d1
unknown <baldwinj@APPA105E21.fvtc.edu>
2013-01-28 14:15:42 -0600

    practical practice 1

---
+++ 01_28_13/TestPractical1/TestPractical1/Exer1Form.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace TestPractical1
+{
+    partial class Exer1Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPractical = new System.Windows.Forms.Label();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPractical
+            // 
+            this.lblPractical.AutoSize = true;
+            this.lblPractical.Font = new System.Drawing.Font("Matura MT Script Capitals", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPractical.ForeColor = System.Drawing.Color.Red;
+            this.lblPractical.Location = new System.Drawing.Point(96, 62);
+            this.lblPractical.Name = "lblPractical";
+            this.lblPractical.Size = new System.Drawing.Size(71, 32);
+            this.lblPractical.TabIndex = 0;
+            this.lblPractical.Text = "label1";
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.Font = new System.Drawing.Font("Modern No. 20", 5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplay.ForeColor = System.Drawing.Color.Blue;
+            this.lblDisplay.Location = new System.Drawing.Point(96, 110);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(20, 7);
+            this.lblDisplay.TabIndex = 1;
+            this.lblDisplay.Text = "label2";
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisplay.Location = new System.Drawing.Point(19, 203);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(90, 40);
+            this.btnDisplay.TabIndex = 2;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(123, 203);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 40);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(227, 203);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 40);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Exer1Form
+            // 
+            this.AcceptButton = this.btnDisplay;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(384, 262);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.lblPractical);
+            this.Name = "Exer1Form";
+            this.Text = "Practical Test #1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPractical;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ 01_28_13/TestPractical1/TestPractical1/Exer1Form.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestPractical1
+{
+    public partial class Exer1Form : Form
+    {
+        public Exer1Form()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Closes the application
+            Application.Exit();
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            // make the label say "good luck!"
+            lblDisplay.Text = "Good Luck!";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Set the top label to say Practice Practical #1
+            lblPractical.Text = "Practice Practical #1";
+            //clear out the bottom label
+            lblDisplay.Text = "";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //clear out both labels
+            lblPractical.Text = "";
+            lblDisplay.Text = "";
+        }
+    }
+}
