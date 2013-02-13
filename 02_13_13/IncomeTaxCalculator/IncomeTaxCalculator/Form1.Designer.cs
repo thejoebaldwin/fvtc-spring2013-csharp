5f83e5938591dcd5926d186c57de11eeda870ba6
Joe Baldwin <contact@humboldttechgroup.com>
2013-02-13 13:55:18 -0600

    Text changed event, multiple calculations

---
+++ 02_13_13/IncomeTaxCalculator/IncomeTaxCalculator/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace IncomeTaxCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblIncomeTax = new System.Windows.Forms.Label();
+            this.lblNetSalary = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSalary.Location = new System.Drawing.Point(217, 49);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(150, 29);
+            this.txtSalary.TabIndex = 0;
+            this.txtSalary.Text = "40000";
+            this.txtSalary.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtSalary.TextChanged += new System.EventHandler(this.txtSalary_TextChanged);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(68, 224);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(100, 50);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Income Tax Owed";
+            // 
+            // lblIncomeTax
+            // 
+            this.lblIncomeTax.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIncomeTax.Location = new System.Drawing.Point(217, 109);
+            this.lblIncomeTax.Name = "lblIncomeTax";
+            this.lblIncomeTax.Size = new System.Drawing.Size(146, 24);
+            this.lblIncomeTax.TabIndex = 3;
+            this.lblIncomeTax.Text = "<lblIncomeTax>";
+            this.lblIncomeTax.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lblNetSalary
+            // 
+            this.lblNetSalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNetSalary.Location = new System.Drawing.Point(232, 157);
+            this.lblNetSalary.Name = "lblNetSalary";
+            this.lblNetSalary.Size = new System.Drawing.Size(131, 24);
+            this.lblNetSalary.TabIndex = 4;
+            this.lblNetSalary.Text = "<lblNetSalary>";
+            this.lblNetSalary.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(98, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Net Salary";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(132, 49);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 24);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Salary";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(200, 224);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 50);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(332, 224);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 50);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnSubmit;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(484, 322);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblNetSalary);
+            this.Controls.Add(this.lblIncomeTax);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtSalary);
+            this.Name = "Form1";
+            this.Text = "Income Tax Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblIncomeTax;
+        private System.Windows.Forms.Label lblNetSalary;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ 02_13_13/IncomeTaxCalculator/IncomeTaxCalculator/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IncomeTaxCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblIncomeTax.Text = "";
+            lblNetSalary.Text = "";
+        }
+
+       
+    
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //Declare variables
+            Double dblSalary = 0;
+            Double dblIncomeTaxOwed = 0;
+            Double dblNetSalary = 0;
+            //get user input
+            dblSalary = Convert.ToDouble(txtSalary.Text);
+            //calculation
+            //   income tax amount = salary x 6.5%
+            dblIncomeTaxOwed = dblSalary * .065;
+            //   net salary = salary - income tax amount
+            dblNetSalary = dblSalary - dblIncomeTaxOwed;
+            //output results to user
+            lblIncomeTax.Text = dblIncomeTaxOwed.ToString("c");
+            lblNetSalary.Text = dblNetSalary.ToString("c");
+
+            txtSalary.Focus();
+            txtSalary.SelectAll();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtSalary.Clear();
+            lblIncomeTax.Text = "";
+            lblNetSalary.Text = "";
+        }
+
+        private void txtSalary_TextChanged(object sender, EventArgs e)
+        {
+            lblIncomeTax.Text = "";
+            lblNetSalary.Text = "";
+        }
+    }
+}
