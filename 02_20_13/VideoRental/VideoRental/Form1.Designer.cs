fe232df017631fa1dfec219c52e76f1949f2154c
Joe Baldwin <contact@humboldttechgroup.com>
2013-02-20 14:16:32 -0600

    Practical Example 2

---
+++ 02_20_13/VideoRental/VideoRental/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace VideoRental
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMovies = new System.Windows.Forms.TextBox();
+            this.txtDays = new System.Windows.Forms.TextBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.Maroon;
+            this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSubmit.Font = new System.Drawing.Font("Copperplate Gothic Bold", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.ForeColor = System.Drawing.Color.White;
+            this.btnSubmit.Location = new System.Drawing.Point(60, 243);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(140, 50);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Copperplate Gothic Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(56, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 50);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "# of Movies";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // txtMovies
+            // 
+            this.txtMovies.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMovies.Location = new System.Drawing.Point(290, 59);
+            this.txtMovies.Name = "txtMovies";
+            this.txtMovies.Size = new System.Drawing.Size(130, 29);
+            this.txtMovies.TabIndex = 0;
+            this.txtMovies.Text = "5";
+            this.txtMovies.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtMovies.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtDays
+            // 
+            this.txtDays.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDays.Location = new System.Drawing.Point(290, 120);
+            this.txtDays.Name = "txtDays";
+            this.txtDays.Size = new System.Drawing.Size(130, 29);
+            this.txtDays.TabIndex = 1;
+            this.txtDays.Text = "3";
+            this.txtDays.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtDays.TextChanged += new System.EventHandler(this.txtDays_TextChanged);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.ForeColor = System.Drawing.Color.White;
+            this.lblTotal.Location = new System.Drawing.Point(280, 182);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(140, 31);
+            this.lblTotal.TabIndex = 4;
+            this.lblTotal.Text = "<lblTotal>";
+            this.lblTotal.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Copperplate Gothic Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(128, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 50);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "# of Days";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Copperplate Gothic Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(128, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 50);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Total Cost";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Maroon;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("Copperplate Gothic Bold", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.White;
+            this.btnClear.Location = new System.Drawing.Point(274, 243);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(120, 50);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Maroon;
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Copperplate Gothic Bold", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(468, 243);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 50);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Copperplate Gothic Bold", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(106, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(370, 32);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "All Movies Are $3/Day";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnSubmit;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(611, 340);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.txtDays);
+            this.Controls.Add(this.txtMovies);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "Form1";
+            this.Text = "Video Rental";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMovies;
+        private System.Windows.Forms.TextBox txtDays;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 02_20_13/VideoRental/VideoRental/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VideoRental
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            lblTotal.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblTotal.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtDays.Clear();
+            txtMovies.Clear();
+            lblTotal.Text = "";
+            txtMovies.Focus();
+
+        }
+
+        private void txtDays_TextChanged(object sender, EventArgs e)
+        {
+            lblTotal.Text = "";
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //declare variables
+            int intMovies = 0;
+            int intDays = 0;
+            Double dblTotal = 0;
+            //get user input
+            //Int32.TryParse(txtMovies.Text, out intMovies);
+            intMovies = Int32.Parse(txtMovies.Text);
+            Int32.TryParse(txtDays.Text, out intDays);
+
+            
+
+            //calculations
+            //  Total Cost = # of movies x # days x $3
+            dblTotal = intMovies * intDays * 3;
+            //output to user
+            lblTotal.Text = dblTotal.ToString("c");
+            //return focus to first textbox
+            txtMovies.Focus();
+            txtMovies.SelectAll();
+        }
+    }
+}
