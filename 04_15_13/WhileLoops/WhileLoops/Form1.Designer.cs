858a86ba3920d106628383b89f7b6c873f4cd0e0
Joe Baldwin <contact@humboldttechgroup.com>
2013-04-15 14:02:54 -0500

    While Loops, Arrays

---
+++ 04_15_13/WhileLoops/WhileLoops/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WhileLoops
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(98, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Loop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.Location = new System.Drawing.Point(98, 128);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(51, 13);
+            this.lblDisplay.TabIndex = 1;
+            this.lblDisplay.Text = "lblDisplay";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(36, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "One at a Time";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 344);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 04_15_13/WhileLoops/WhileLoops/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WhileLoops
+{
+    public partial class Form1 : Form
+    {
+
+        int oneTimeCounter = 0;
+        string[] StudentNames = { "Betty", "Bill", "Rick", "George", "Ron" };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+            //acculator (counter)
+            int i = 0;
+            string strDisplay = "";
+
+            //run this loop until i = 10
+            while (i < StudentNames.Length)
+            {
+                strDisplay += StudentNames[i] + "\n";
+                lblDisplay.Text = strDisplay;
+                i++;
+            }
+
+            lblDisplay.Text = strDisplay;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            string strDisplay = "";
+
+            strDisplay = StudentNames[oneTimeCounter];
+
+
+            lblDisplay.Text = strDisplay;
+
+            oneTimeCounter++;
+            if (oneTimeCounter >= StudentNames.Length)
+            {
+                oneTimeCounter = 0;
+            }
+        }
+    }
+}
