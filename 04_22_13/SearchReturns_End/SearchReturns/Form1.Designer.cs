9747b3e7e1f0ee05c0d94e1486d0085a909a3c7d
Joe Baldwin <contact@humboldttechgroup.com>
2013-04-22 14:17:29 -0500

    Multiple Arrays & Array Search

---
+++ 04_22_13/SearchReturns_End/SearchReturns/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace SearchReturns
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbStudents = new System.Windows.Forms.ListBox();
+            this.btnTotal = new System.Windows.Forms.Button();
+            this.lblBalance = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbStudents
+            // 
+            this.lbStudents.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbStudents.FormattingEnabled = true;
+            this.lbStudents.ItemHeight = 24;
+            this.lbStudents.Location = new System.Drawing.Point(14, 89);
+            this.lbStudents.Name = "lbStudents";
+            this.lbStudents.Size = new System.Drawing.Size(120, 76);
+            this.lbStudents.TabIndex = 0;
+            this.lbStudents.SelectedIndexChanged += new System.EventHandler(this.lbStudents_SelectedIndexChanged);
+            // 
+            // btnTotal
+            // 
+            this.btnTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTotal.Location = new System.Drawing.Point(110, 240);
+            this.btnTotal.Name = "btnTotal";
+            this.btnTotal.Size = new System.Drawing.Size(100, 50);
+            this.btnTotal.TabIndex = 1;
+            this.btnTotal.Text = "Total";
+            this.btnTotal.UseVisualStyleBackColor = true;
+            this.btnTotal.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblBalance
+            // 
+            this.lblBalance.AutoSize = true;
+            this.lblBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBalance.Location = new System.Drawing.Point(251, 100);
+            this.lblBalance.Name = "lblBalance";
+            this.lblBalance.Size = new System.Drawing.Size(119, 24);
+            this.lblBalance.TabIndex = 2;
+            this.lblBalance.Text = "<lblBalance>";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(258, 181);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(92, 24);
+            this.lblTotal.TabIndex = 3;
+            this.lblTotal.Text = "<lblTotal>";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(136, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Balance:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(164, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Total:";
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAverage.Location = new System.Drawing.Point(218, 239);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(100, 50);
+            this.btnAverage.TabIndex = 6;
+            this.btnAverage.Text = "Average";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            this.btnAverage.Click += new System.EventHandler(this.btnAverage_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(163, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 24);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Average";
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverage.Location = new System.Drawing.Point(257, 208);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(122, 24);
+            this.lblAverage.TabIndex = 7;
+            this.lblAverage.Text = "<lblAverage>";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(151, 26);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 9;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(273, 26);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 10;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(64, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Name:";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnSearch;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 318);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblBalance);
+            this.Controls.Add(this.btnTotal);
+            this.Controls.Add(this.lbStudents);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbStudents;
+        private System.Windows.Forms.Button btnTotal;
+        private System.Windows.Forms.Label lblBalance;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ 04_22_13/SearchReturns_End/SearchReturns/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SearchReturns
+{
+    public partial class Form1 : Form
+    {
+
+        private string[] Students = {"John", "Stacy", "Tony", "Jeff", "Rick",
+                                    "Tom", "Peter", "Tanya", "Betty", "Suzie",
+                                     "Ronald", "Roxanne", "Keith", "Ken", "Liz",
+                                     "James", "Pattty", "Paul", "Judd", "Ashley" };
+        
+        private int[] SavingsBalance = {0, 200, 10000, 12000, 7000,
+                                        340, 56, 45, 56, 800, 
+                                        9400, 800, 6400, 5600, 4800, 
+                                        45, 560, 28 , 0, 20};
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int counter = 0;
+            while (counter < Students.Length)
+            {
+                lbStudents.Items.Add(Students[counter]);
+                counter++;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblTotal.Text = getTotal().ToString("c");
+        }
+
+        private void lbStudents_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int intSelectedIndex = lbStudents.SelectedIndex;
+            lblBalance.Text = SavingsBalance[intSelectedIndex].ToString("c");
+        }
+
+        private int getTotal()
+        {
+            int intTotal = 0;
+            //loop goes here
+            int counter = 0;
+            while (counter < SavingsBalance.Length)
+            {
+                intTotal += SavingsBalance[counter];
+                counter++;
+            }
+            return intTotal;
+        }
+
+        private void btnAverage_Click(object sender, EventArgs e)
+        {
+            int intTotal = getTotal();
+            double dblAverage = intTotal / SavingsBalance.Length;
+            lblAverage.Text = dblAverage.ToString("c");
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+            int intSelectedIndex = -1;
+            string strSearch = txtName.Text.ToUpper();
+            while (counter < Students.Length)
+            {
+                if (strSearch == Students[counter].ToUpper())
+                {
+                    intSelectedIndex = counter;
+                    break;
+                }
+                counter++;
+            }
+
+            if (intSelectedIndex > -1)
+            {
+                lbStudents.SelectedIndex = intSelectedIndex;
+            }
+
+        }
+    }
+}
