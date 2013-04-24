0b3c429a111e34ea3f7fe7acbccd32c5e86dbd3e
Joe Baldwin <contact@humboldttechgroup.com>
2013-04-24 13:45:31 -0500

    Multiply accumulator/for loops

---
+++ 04_24_13/CareerPredictor/CareerPredictor/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace CareerPredictor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPredict = new System.Windows.Forms.Button();
+            this.lbSalary = new System.Windows.Forms.ListBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.txtRaise = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbltotal = new System.Windows.Forms.Label();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnPredict
+            // 
+            this.btnPredict.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPredict.Location = new System.Drawing.Point(349, 196);
+            this.btnPredict.Name = "btnPredict";
+            this.btnPredict.Size = new System.Drawing.Size(130, 40);
+            this.btnPredict.TabIndex = 0;
+            this.btnPredict.Text = "&Predict!";
+            this.btnPredict.UseVisualStyleBackColor = true;
+            this.btnPredict.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbSalary
+            // 
+            this.lbSalary.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSalary.FormattingEnabled = true;
+            this.lbSalary.ItemHeight = 26;
+            this.lbSalary.Items.AddRange(new object[] {
+            "Year 1: $45000",
+            "Year 2: $46350",
+            "Year 3: $47740",
+            "Year 4: $49172",
+            "Year 5: $50647"});
+            this.lbSalary.Location = new System.Drawing.Point(9, 16);
+            this.lbSalary.Name = "lbSalary";
+            this.lbSalary.Size = new System.Drawing.Size(250, 238);
+            this.lbSalary.TabIndex = 1;
+            this.lbSalary.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSalary.Location = new System.Drawing.Point(409, 42);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(150, 33);
+            this.txtSalary.TabIndex = 2;
+            this.txtSalary.Text = "45000";
+            this.txtSalary.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtSalary.TextChanged += new System.EventHandler(this.txtSalary_TextChanged);
+            // 
+            // txtRaise
+            // 
+            this.txtRaise.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRaise.Location = new System.Drawing.Point(409, 85);
+            this.txtRaise.Name = "txtRaise";
+            this.txtRaise.Size = new System.Drawing.Size(150, 33);
+            this.txtRaise.TabIndex = 3;
+            this.txtRaise.Text = "3";
+            this.txtRaise.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtRaise.TextChanged += new System.EventHandler(this.txtRaise_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(293, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Base Salary";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(270, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 26);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "% Yearly Raise";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(333, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 26);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Total $";
+            // 
+            // lbltotal
+            // 
+            this.lbltotal.AutoSize = true;
+            this.lbltotal.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltotal.Location = new System.Drawing.Point(412, 166);
+            this.lbltotal.Name = "lbltotal";
+            this.lbltotal.Size = new System.Drawing.Size(94, 26);
+            this.lbltotal.TabIndex = 7;
+            this.lbltotal.Text = "<lblTotal>";
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnQuit.FlatAppearance.BorderSize = 0;
+            this.btnQuit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQuit.Location = new System.Drawing.Point(572, 225);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(10, 10);
+            this.btnQuit.TabIndex = 8;
+            this.btnQuit.Text = "E&xit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(339, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 26);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Years";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(411, 130);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 33);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "10";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnPredict;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnQuit;
+            this.ClientSize = new System.Drawing.Size(578, 262);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.lbltotal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRaise);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.lbSalary);
+            this.Controls.Add(this.btnPredict);
+            this.Name = "Form1";
+            this.Text = "Career Predictor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPredict;
+        private System.Windows.Forms.ListBox lbSalary;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.TextBox txtRaise;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbltotal;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ 04_24_13/CareerPredictor/CareerPredictor/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CareerPredictor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbSalary.Items.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbSalary.Items.Clear();
+            //declare variables
+            double dblBaseSalary = 0;
+            double dblOldSalary = 0;
+            double dblRaise = 0;
+            double dblTotal = 0;
+            double dblYear = 0;
+
+            int counter = 0;
+            //get input
+            Double.TryParse(txtSalary.Text, out dblBaseSalary);
+            Double.TryParse(txtRaise.Text, out dblRaise);
+            Double.TryParse(textBox1.Text, out dblYear);
+            dblRaise = 1 + (dblRaise / 100);
+            //logic!
+            //  start with base salary
+            //  loop 5 times
+            //  new salary = old salary * 1.03
+            //  output new salary
+            dblOldSalary = dblBaseSalary;
+
+            for (int i = 0; i < dblYear; i++)
+            {
+                string strDisplay = "Year " + (i + 1).ToString() + ":" + dblOldSalary.ToString("c");
+                lbSalary.Items.Add(strDisplay);
+                Double dblNewSalary = dblOldSalary * dblRaise;
+                dblTotal += dblOldSalary;
+                dblOldSalary = dblNewSalary;
+            }
+
+            lbSalary.SelectedIndex = lbSalary.Items.Count - 1;
+
+            //while (counter < dblYear)
+            //{
+            //    string strDisplay = "Year " + (counter + 1).ToString() + ":" + dblOldSalary.ToString("c");
+            //    lbSalary.Items.Add(strDisplay);
+            //    Double dblNewSalary = dblOldSalary * dblRaise;
+            //    dblTotal += dblOldSalary;
+            //    dblOldSalary = dblNewSalary;
+            //    counter++;
+            //}
+            lbltotal.Text = dblTotal.ToString("c");
+            txtSalary.Focus();
+            txtSalary.SelectAll();
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txtRaise_TextChanged(object sender, EventArgs e)
+        {
+            lbSalary.Items.Clear();
+        }
+
+        private void txtSalary_TextChanged(object sender, EventArgs e)
+        {
+            lbSalary.Items.Clear();
+        }
+    }
+}
