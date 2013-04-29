bf37d427b476fde6d349507aa2dc1c893867d2b8
Joe Baldwin <contact@humboldttechgroup.com>
2013-04-29 13:48:17 -0500

    Do While, More Arrays

---
+++ 04_29_13/DoWhile/DoWhile/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DoWhile
+{
+    class Program
+    {
+        const string PASSWORD = "";
+
+        static void Main(string[] args)
+        {
+            string strInput = string.Empty;
+            do
+            {
+                //password is wrong
+                Console.WriteLine("Please enter your password:");
+                strInput = Console.ReadLine();
+            } while (strInput != PASSWORD);
+
+            //if outside the while loop, 
+            //  then they guessed correctly
+            Console.WriteLine("You entered the correct password!");
+            Console.Write("Press Any Key to Continue:");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 04_29_13/Registrar/Registrar/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Registrar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intMenu = 0;
+            string[] Classes = {
+                                   "Intro to C#",
+                                   "C# Intermediate",
+                                   "Advanced Programming",
+                                   "Project Delivery"
+                               };
+
+            int[] Seats = {
+                              0,
+                              4,
+                              5,
+                              2
+                          };
+
+
+            while (true)
+            {
+
+                for (int i = 0; i < Classes.Length; i++)
+                {
+                    Console.WriteLine(i.ToString() + ":" + Classes[i]);
+                }
+                //this is the exact same thing
+                //int i = 0;
+                //while (i < Classes.Length)
+                //{
+                //    i++;
+                //}
+
+
+
+                string strInput = string.Empty;
+                do
+                {
+                    Console.Write("Please select an option:");
+                    strInput = Console.ReadLine();
+
+                } while (!Int32.TryParse(strInput, out intMenu)  || intMenu >= Classes.Length);
+
+                Console.WriteLine(Classes[intMenu] + " has " + Seats[intMenu] + " seats");
+
+                Console.Write("Press the enter key to quit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
