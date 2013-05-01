7e037b65cb54314d750d47dafa54b7086152913c
Joe Baldwin <contact@humboldttechgroup.com>
2013-05-01 13:53:41 -0500

    repetition structure review

---
+++ 05_01_13/Review/Review/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Review
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxPrice = new System.Windows.Forms.ListBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.btnBuy = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbxPrice
+            // 
+            this.lbxPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxPrice.FormattingEnabled = true;
+            this.lbxPrice.ItemHeight = 24;
+            this.lbxPrice.Location = new System.Drawing.Point(43, 93);
+            this.lbxPrice.Name = "lbxPrice";
+            this.lbxPrice.Size = new System.Drawing.Size(120, 76);
+            this.lbxPrice.TabIndex = 0;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuantity.Location = new System.Drawing.Point(119, 51);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(100, 29);
+            this.txtQuantity.TabIndex = 1;
+            // 
+            // btnBuy
+            // 
+            this.btnBuy.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuy.Location = new System.Drawing.Point(80, 198);
+            this.btnBuy.Name = "btnBuy";
+            this.btnBuy.Size = new System.Drawing.Size(120, 50);
+            this.btnBuy.TabIndex = 2;
+            this.btnBuy.Text = "Buy While";
+            this.btnBuy.UseVisualStyleBackColor = true;
+            this.btnBuy.Click += new System.EventHandler(this.btnBuy_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Quantity";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(51, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "All Tickets are $9.50";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(174, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 50);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Buy For";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBuy);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.lbxPrice);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxPrice;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Button btnBuy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 05_01_13/Review/Review/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Review
+{
+    public partial class Form1 : Form
+    {
+
+        const double MOVIE_PRICE = 9.50;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //a
+            int i = 0;
+            //b
+            int dtotal = 0;
+            //c
+            do
+            {
+                //d
+                dtotal = dtotal + Int32.Parse(lbxPrice.SelectedItem.ToString());
+                //e
+                i++;
+            } while (i < lbxPrice.Items.Count);
+
+            //a) 0
+            //b) 1
+            //c) 2
+            //d) 3
+            //e) 4
+            
+        }
+
+        private void btnBuy_Click(object sender, EventArgs e)
+        {
+
+
+            int intQuantity = 0;
+            Int32.TryParse(txtQuantity.Text, out intQuantity);
+            int i = 0;
+            while (i < intQuantity)
+            {
+                //multiply the price times the iteration (plus one)
+                double dblTotal = (i + 1) * MOVIE_PRICE;
+                lbxPrice.Items.Add(dblTotal.ToString("c"));
+                i++;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int intQuantity = 0;
+            Int32.TryParse(txtQuantity.Text, out intQuantity);
+            double dblTotal = 0;
+            for (int i = 0; i < intQuantity; i++)
+            {
+                //accumulate the price each iteration
+                dblTotal += MOVIE_PRICE;
+                lbxPrice.Items.Add(dblTotal.ToString("c"));
+            }
+        }
+    }
+}
